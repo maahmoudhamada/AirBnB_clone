c46dd3d818bd787e805d1913d8dd63569cfa8d09
maahmoudhamada <maahmoud.hamada@hotmail.com>
2024-06-09 05:44:48 +0300

    Frist Commit

---
+++ models/base_model.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""BaseModel Module"""
+
+
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+    """BaseModel Class"""
+
+    def __init__(self):
+        """Constructor method"""
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    # =========================== Str ================================
+
+    def __str__(self):
+        """Mehtod to return unformal string repr. for an instance"""
+        return "[{}] ({}) {}" \
+            .format(self.__class__.__name__, self.id, self.__dict__)
+
+    # =========================== Save ==============================
+
+    def save(self):
+        """Method to update updated_at attribute"""
+        self.updated_at = datetime.now()
+
+    # =========================== to_dict ===========================
+
+    def to_dict(self):
+        """Method to return a copy from an instance dict"""
+        new_dict = self.__dict__.copy()
+        crtd_at = new_dict['created_at'].isoformat()
+        uptd_at = new_dict['updated_at'].isoformat()
+        new_dict.update({'__class__': self.__class__.__name__})
+        new_dict.update({'created_at': crtd_at})
+        new_dict.update({'updated_at': uptd_at})
+        return new_dict
---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Test module fro BaseModel class"""
+
+from models.base_model import BaseModel
+from datetime import datetime
+import unittest
+
+
+class TestBaseModel(unittest.TestCase):
+    """Class to test BaseModel class"""
+
+    def test_attributes(self):
+        """Method to test BaseModel class attributes"""
+        b = BaseModel()
+        self.assertIsInstance(b.id, str)
+        self.assertIsInstance(b.created_at, datetime)
+        self.assertIsInstance(b.updated_at, datetime)
+
+    def test_str(self):
+        """Method to test BaseModel's str method"""
+        b = BaseModel()
+        st = b.__str__()
+        out = "[{}] ({}) {}".format(b.__class__.__name__, b.id, b.__dict__)
+        self.assertIsInstance(st, str)
+        self.assertEqual(out, st)
+
+    def save(self):
+        """Method to test BaseModel's save method"""
+        pass
+
+    def test_to_dict(self):
+        """Method to test BaseModel's to_dict method"""
+        b = BaseModel()
+        dic = b.to_dict()
+        self.assertIsInstance(dic, dict)
+        self.assertIsInstance(dic['updated_at'], str)
+        self.assertIsInstance(dic['created_at'], str)

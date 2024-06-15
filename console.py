f14d20cbaca02fd60c1479a19ff1ac10204178d0
maahmoudhamada <maahmoud.hamada@hotmail.com>
2024-06-15 10:23:34 +0300

    Destroy + all

--- console.py
+++ console.py
@@ -59,6 +59,53 @@ class HBNBCommand(cmd.Cmd):
             else:
                 print(objs[key])
 
+    def do_destroy(self, line):
+        """Method to delete (destroy) an instance"""
+        tmp = line.split()
+        _class = None
+        id = None
+        try:
+            _class = tmp[0]
+            id = tmp[1]
+        except IndexError:
+            pass
+        if not line:
+            print("** class name missing **")
+        elif _class not in self.classess:
+            print(_class)
+            print("** class doesn't exist **")
+        elif not id:
+            print("** instance id missing **")
+        else:
+            key = "{}.{}".format(_class, id)
+            objs = storage.all()
+            if key not in objs:
+                print("** no instance found **")
+            else:
+                storage.all().pop(key)
+                storage.save()
+
+    def do_all(self, line):
+        """Method to print all str repr of all objs"""
+        objs = storage.all()
+        lst = []
+        for value in objs.values():
+            lst.append(str(value))
+        if not line:
+            print(lst)
+        else:
+            tmp = line.split()
+            _class = None
+            try:
+                _class = tmp[0]
+            except IndexError:
+                pass
+
+            if _class and _class in self.classess:
+                print(lst)
+            else:
+                print("** class doesn't exist **")
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
